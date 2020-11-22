9067ac8ade4c6ecfa8d732e76f38ded425c60892
Your Name <you@example.com>
2020-11-22 13:32:21 +0000

    Store any artifacts copied to ${OB_ARTIFACTS_DIR} by wildfly-core-ts-smoke

---
+++ repo-backups/wildfly/org/wildfly/wildfly-dist-legacy/22.0.0.Beta1-SNAPSHOT/wildfly-dist-legacy-22.0.0.Beta1-SNAPSHOT.zip.split.file.dir/reassemble.sh
@@ -0,0 +1,4 @@
+#!/bin/sh# The file has been split due to its large size. Run this script to reassemble it.
+WORKING_DIR=$(dirname $0)
+cat ${WORKING_DIR}/xx.?? > ${WORKING_DIR}/wildfly-dist-legacy-22.0.0.Beta1-SNAPSHOT.zip
+echo Reassembled ${WORKING_DIR}/wildfly-dist-legacy-22.0.0.Beta1-SNAPSHOT.zip
